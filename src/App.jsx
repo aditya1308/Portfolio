5ebad8e2e1108b4eba3efabb9be2a1a4760dc1ab
Aditya Prasad <asmarty2016@gmail.com>
2025-01-25 21:20:52 +0530

    Routing changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import { BrowserRouter ,Routes, Route} from 'react-router-dom';
+import { HashRouter ,Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import Experience from './components/Experience';
+import NotFound from './NotFound';
+import About from './components/About';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Navbar/>
       <Routes>
         <Route path='/Portfolio' element={<Home/>}/>
         <Route path='/experience' element={<Experience/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer/>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
