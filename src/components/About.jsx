2f52f55702bac79c3cd5d0eee665257d03c4550b
Aditya Prasad <asmarty2016@gmail.com>
2025-12-26 00:53:04 +0530

    Fix for drive link

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -38,7 +38,7 @@ const About = () => {
             published on <a href='https://www.geeksforgeeks.org/user/adityaprasad1308/contributions/' style={{ color: 'yellow' }}>GeeksForGeeks</a> website.</p>
 
             <ButtonLink
-              url='https://drive.google.com/file/d/1XEn2UjPjXALPkrspkSSjaER5g1T8Ac7O/view?usp=sharing'
+              url='https://drive.google.com/file/d/1gnb2T4m9maY0Le0svH8eMm620ZipS_DQ/view?usp=sharing'
               text='Resume →'
               padding={`p-3`}
             />
